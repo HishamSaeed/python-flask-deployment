a4a1905142e2f8810784888d1e397facf9f7b367
HishamSaeed <hisham.saeed.92@gmail.com>
2024-03-15 15:14:19 +0100

    added CORS and endpoint

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, world! , this is just a demo for flask . This is a demo for fenicsWeb'
+
+@app.route('/t_start')
+def getTStart():
+    return jsonify({ "tStart": 32 })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
